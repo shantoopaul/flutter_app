2197e7b04c022b0e66ef090e291eea66a0a4aed6
shantoopaul <shantoopaul@gmail.com>
2026-06-21 21:10:12 +0600

    refactor: removed unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -4,67 +4,39 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.teal,
+          seedColor: Colors.indigo,
           brightness: Brightness.dark,
         ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter App'),
-        centerTitle: true,
-        backgroundColor: Colors.teal,
-      ),
-
-      body: currentIndex == 1
-          ? Center(
-              child: Text(
-                'Profile',
-                style: TextStyle(color: Colors.teal, fontSize: 50.0),
-              ),
-            )
-          : Center(
-              child: Text(
-                'Home Page',
-                style: TextStyle(color: Colors.teal, fontSize: 50.0),
-              ),
-            ),
-
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        selectedIndex: currentIndex,
-        onDestinationSelected: (value) {
-          setState(() {
-            currentIndex = value;
-          });
-        },
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter App'), centerTitle: true),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+        ),
       ),
     );
   }
