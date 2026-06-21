01f94475f1a5cb0772005fd76f09a2c449f85721
shantoopaul <shantoopaul@gmail.com>
2026-06-21 20:52:54 +0600

    add: simple stateful function

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-// Stateless (stl)
-// MaterialApp
-// Scaffold
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -21,18 +17,54 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Flutter App'),
-          centerTitle: true,
-          backgroundColor: Colors.teal,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+
+      body: currentIndex == 1
+          ? Center(
+              child: Text(
+                'Profile',
+                style: TextStyle(color: Colors.teal, fontSize: 50.0),
+              ),
+            )
+          : Center(
+              child: Text(
+                'Home Page',
+                style: TextStyle(color: Colors.teal, fontSize: 50.0),
+              ),
+            ),
+
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        selectedIndex: currentIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
       ),
     );
   }
